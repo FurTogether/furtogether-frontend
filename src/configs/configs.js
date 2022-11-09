58eac9123a2544c117ade10ab97e19eb79f5d5cf
andrewlimhj <andrewlim.hj@gmail.com>
2022-11-09 22:49:57 +1100

    changed the api url

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -5,8 +5,8 @@
 const config = {
   // eslint-disable-next-line no-undef
   env: process.env.REACT_APP_ENV || 'production',
-  // apiUrl: `https://furtogether.fly.dev`,
-  apiUrl: `http://localhost:3004`,
+  apiUrl: `https://furtogether.fly.dev`,
+  // apiUrl: `http://localhost:3004`,
   // apiUrl: 'https://furtogether-dev.fly.dev'
 };
 
