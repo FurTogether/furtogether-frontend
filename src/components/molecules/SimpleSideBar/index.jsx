b57d1a3d3a30dd1a93a1fdd33264b07d6a694ace
richiechia <richiechia@gmail.com>
2022-11-09 15:37:41 +0800

    Delete comments

--- src/components/molecules/SimpleSideBar/index.jsx
+++ src/components/molecules/SimpleSideBar/index.jsx
@@ -40,23 +40,6 @@ const SimpleSideBar = ({eventHandler, data}) => {
             <ChakraCheckBox eventHandler={eventHandler} data={data} direction={'column'}/>
           </AccordionPanel>
         </AccordionItem>
-
-        {/* <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex='1' textAlign='left'>
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </AccordionPanel>
-        </AccordionItem> */}
       </Accordion>
       
     </Flex>
